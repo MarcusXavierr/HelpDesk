38686de112caedcb11bed3ca5e86aa7bd2816aa0
Marcus Xavier <marcusxavierr123@gmail.com>
2020-06-22 11:14:05 -0300

    improve code

--- utils/db_connection.php
+++ utils/db_connection.php
@@ -1,10 +1,14 @@
 <?php
-$dsn = 'mysql:host=remotemysql.com:3306/;dbname=FulGmClGlR';
-$db_username = 'FulGmClGlR';
-$db_password = 'jPDKaq2RX6';
-try{
+$dsn = 'mysql:host=host:3306/;dbname=db_name';
+$db_username = 'username';
+$db_password = 'your_pass';
+
+try
+{
     $connection = new PDO($dsn,$db_username,$db_password);
-}catch(PDOException $e){
+}
+catch(PDOException $e)
+{
     print_r($e);
 }
 
